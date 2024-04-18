0e962edf0b749b841ccbf3385abf760209772fd8
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-04-18 18:33:18 +0530

    ml_clf_app.py

--- ml_clf_app.py
+++ ml_clf_app.py
@@ -91,7 +91,7 @@ st.set_page_config(page_title="Classification App | v1.0",
 st.title("ML | Classification App | v1.0")
 st.markdown('Created by | <a href="mailto:avijit.mba18@gmail.com">Avijit Chakraborty</a>', 
             unsafe_allow_html=True)
-st.info('**Disclaimer : Unauthorized uses or copying of the app is strictly prohibited.**', icon="ℹ️")
+st.info('**Disclaimer : :blue[Thank you for visiting the app] | Unauthorized uses or copying of the app is strictly prohibited | Please expand the below :blue[Knowledge] tab to know more and click the :blue[sidebar] to follow the instructions to start the applications.**', icon="ℹ️")
 #----------------------------------------
 # Set the background image
 st.divider()
